12e586568c86565fa680f8c4665f8fdfc5070dba
evansalfahmi-cloud <evansalfahmi.org@gmail.com>
2023-04-07 17:17:03 +0700

    menggunakan konstruktor

--- Program.cs
+++ Program.cs
@@ -1,74 +1,50 @@
 ﻿/*
-METODE
-fungsi yang dibuat didalam kelas untuk melakukan tugas spesifik tertentu
+kONSTRUKTOR
 
-2 Jenis
-Nilai balik
-tanpa nilai balik
-
-==========================
-METODE TANPA NILAI BALIK
-=========================
-
-PENDEFINISIAN MENGGUNAKAN KATA KUNCI VOID
-Tipe akses vooid NAMAMETODE(daftar-parameter){
-    //badan metode
-}
-
-==========================
-METODE DENGAN NILAI BALIK
-==========================
-PENDEFINISIAN
-TipeAkses TipeKembalian NamaMetode(daftar-parameter){
-    badan metode;
-    return nilai;
-
-}
-
-METODE PARAMETER
-
-PARAMETER : NILAI YANG DIMASUKKAN KE DALAM BADAN METODE
-
-PERLU DIKETAHUI BAHWA PARAMETER YANG DIGUNAKAN DALAM PENDEFINISIAN METODE DISEBUT DENGAN PARAMETERFORMAL ATAU PARAMETER SAJA
-SEDANGKAN NILAI PARAMETER YANG DISERTAKAN SAAT PEMANGGILAN METODE DISEBUT DENGAN ARGUMEN
-
-karena metode ini akan mengembalikan nilai maka perlu di deklarasikan menggunakan tipe data
-
+Metode khusus yang namanya sama dengan nama kelas dan akan dipanggil secara otomatis ketika proses pembuatan objek
+contoh :
 */
-using System; 
+using System;
 
 class persegiPanjang {
-    public double panjang;
+    public double panjang ;
     public double lebar;
 
-    //mendefinisikan metode dengan dua parameter
-    public void isiData (double p, double l){
-        panjang = p;
-        lebar = l;        
-    }
-    //mendefinisikan metode untuk meghitung luas
-    public double rumuasLuas(){
-        return panjang * lebar ; 
+    //mendefinisikan konstruktor
+    public persegiPanjang(){
+        panjang = 8; 
+        lebar =  9;
     }
 
-    //mendefinisian metode untuk menampilkan nilai luas
+    //mendefinisikan metode untuk ubah data
+    public void ubahData ( double p , double l ){
+        panjang = p;
+        lebar = l;
+    }
+
+    //mendefinisikan metode untuk menghitung luas
+    public double rumusLuas ()
+    {
+        return panjang * lebar;   
+    }
+
+    //mendifinisikan tampilkan hasil luas 
     public void tampilkanLuas (string s){
-        Console.WriteLine (s + rumuasLuas());
+        Console.WriteLine(s + rumusLuas());
     }
 }
 
-class demoMetodeParameter {
-    static void Main (){
-        persegiPanjang perPjg1 = new persegiPanjang(); // buat objek persegi panjang 1
-        persegiPanjang perPjg2 = new persegiPanjang(); // buat objek persegi panjang 2
-        
-        perPjg1.isiData(7,6); // mengisi data persegi panjang 1 panjang dan lebar
-        perPjg2.isiData(8,9); // mengisi data persegi panjang 2 panjang dan lebar
+class konstruktorDemo {
+    static void Main () {
+        persegiPanjang perPjg = new persegiPanjang ();
 
-        //menampilkan nilai luas
-        perPjg1.tampilkanLuas("luas persegi panjang 1 adalah = ");
-        perPjg2.tampilkanLuas("luas persegi panjang 2 adalah = ");
-        
+        //menampilkan luas dengan data awal
+        perPjg.tampilkanLuas("Luas awal adalah = ");
 
+        //merubah data panjang dan lebar
+        perPjg.ubahData(13,7);
+
+        //menampilkan luas dengan data baru
+        perPjg.tampilkanLuas("Luas akhir adalah = ");
     }
 }
